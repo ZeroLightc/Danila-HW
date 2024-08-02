b143a8cce4cdbcfb67aaa9bd7acc2422eaebf05b
ZeroLightc <budan0305@mail.ru>
2024-08-02 15:56:47 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+#1st program
+print(9**0.5*5)
+
+#2nd program
+print(9.99>9.98, 1000!=1000.1)
+
+#3rd program
+print(2+2*2)
+print((2+2)*2)
+print((2+2*2)==((2+2)*2))
+
+#4th program
+x = float('123.456')
+x1 = (abs(int((x - x*10))))
+print(str(x).split('.')[1][0])
